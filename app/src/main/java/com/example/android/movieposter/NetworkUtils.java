0e2685f2a18f671c9f15a8dbd796c8e1fa352db2
matewiszt <matezoltan.wiszt@gmail.com>
2018-02-21 22:06:27 +0100

    Add NetworkUtils class and utility methods to handle HTTP requests

---
+++ app/src/main/java/com/example/android/movieposter/NetworkUtils.java
@@ -0,0 +1,127 @@
+package com.example.android.movieposter;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class NetworkUtils {
+
+    //Tag constant for logging
+    private static final String LOG_TAG = "NetworkUtils: ";
+
+    //Public constructor which will be never used
+    public NetworkUtils(){}
+
+
+    /*
+     * Get the response from an InputStream
+     * @param stream: the InputStream that we want to read the response from
+     * @return String: the String read from the InputStream
+     */
+
+    private static String readFromStream(InputStream stream) throws IOException{
+
+        StringBuilder responseString = new StringBuilder();
+
+        //If we don't have an input stream, we cannot get the response String
+        if (stream == null){
+            return null;
+        }
+
+        //Create a new InputStreamReader and a BufferedReader
+        InputStreamReader streamReader = new InputStreamReader(stream, Charset.forName("UTF-8"));
+        BufferedReader bufferedReader = new BufferedReader(streamReader);
+
+        //Read the buffered response from line to line and append it to the StringBuilder
+        String line = bufferedReader.readLine();
+        while(line != null){
+            responseString.append(line);
+            line = bufferedReader.readLine();
+        }
+
+        //Convert the StringBuilder to a String and return it
+        return responseString.toString();
+
+    }
+
+    /*
+     * Make an HTTP request
+     * @param url: the URL to which we want to send a request
+     * @return String: the response String (unparsed JSON)
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+
+        String responseString = null;
+
+        //If we don't have URL, we won't have a response, so return null
+        if (url == null) {
+            return null;
+        }
+
+        HttpURLConnection connection = null;
+        InputStream responseStream = null;
+
+        try {
+
+            //Set the parameters of the connection and connect
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(15000);
+            connection.setReadTimeout(15000);
+            connection.connect();
+
+            if (connection.getResponseCode() == 200){
+
+                //If the connection is successful, get the input stream and read the response String from it
+                responseStream = connection.getInputStream();
+                responseString = readFromStream(responseStream);
+            } else {
+                Log.e(LOG_TAG, "Server error");
+            }
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Connection creation failed", e);
+        } finally {
+
+            //If we are not disconnected yet, disconnect
+            if (connection != null){
+                connection.disconnect();
+            }
+
+            //If the input stream is not closed yet, close it
+            if (responseStream != null){
+                responseStream.close();
+            }
+
+        }
+
+        return responseString;
+
+    }
+
+    /*
+     * Create a URL from a String
+     * @param urlString: the String from which we want to create a URL
+     * @return URL: the properly formatted URL
+     */
+    private static URL createUrl(String urlString){
+        URL url = null;
+
+        try {
+            url = new URL(urlString);
+
+        } catch (MalformedURLException e){
+            Log.e(LOG_TAG, "URL creation failed", e);
+        }
+        return url;
+
+    }
+
+}
