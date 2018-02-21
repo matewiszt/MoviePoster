24327b889367277d999a75ee697c2b3f5803f4a4
matewiszt <matezoltan.wiszt@gmail.com>
2018-02-21 22:36:47 +0100

    Add Picasso to gradle file, internet permission to manifest and create basic constants for API calls

--- app/src/main/java/com/example/android/movieposter/MainActivity.java
+++ app/src/main/java/com/example/android/movieposter/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.movieposter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String LOG_TAG = getClass().getName();
+
+    private static final String API_KEY = BuildConfig.MOVIEDB_API_KEY;
+    private static final String BASE_URL = "https://api.themoviedb.org/3/";
+    private static final String API_ENDPOINT_TOP_RATED = "movie/top_rated";
+    private static final String API_ENDPOINT_MOVIE = "movie/";
+    private static final String LANGCODE = "en-US";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
     }
 }
