841aca2c5a3bbfdc8ed14d361907830d9cde33f8
matewiszt <matezoltan.wiszt@gmail.com>
2018-02-21 22:05:01 +0100

    Add new Movie class with a public constructor and getter and setter methods

---
+++ app/src/main/java/com/example/android/movieposter/Movie.java
@@ -0,0 +1,64 @@
+package com.example.android.movieposter;
+
+/*
+ * Movie class to store data related to one movie
+ */
+
+public class Movie {
+
+    private String mOriginalTitle;
+    private String mThumbnailImagePath;
+    private String mSynopsis;
+    private double mUserRating;
+    private String mReleaseDate;
+
+    //Public constructor for an instance of the Movie class
+    public Movie(String title, String imagePath, String synopsis, double rating, String releaseDate) {
+        mOriginalTitle = title;
+        mThumbnailImagePath = imagePath;
+        mSynopsis = synopsis;
+        mUserRating = rating;
+        mReleaseDate = releaseDate;
+    }
+
+    //Getter and setter methods for every property
+    public String getTitle(){
+        return mOriginalTitle;
+    }
+
+    public void setTitle(String newTitle){
+        mOriginalTitle = newTitle;
+    }
+
+    public String getImagePath(){
+        return mThumbnailImagePath;
+    }
+
+    public void setImagePath(String newImagePath){
+        mThumbnailImagePath = newImagePath;
+    }
+
+    public String getSynopsis(){
+        return mSynopsis;
+    }
+
+    public void setSynopsis(String newSynopsis){
+        mSynopsis = newSynopsis;
+    }
+
+    public double getRating(){
+        return mUserRating;
+    }
+
+    public void setRating(double newRating){
+        mUserRating = newRating;
+    }
+
+    public String getReleaseDate(){
+        return mReleaseDate;
+    }
+
+    public void setReleaseDate(String newDate){
+        mReleaseDate = newDate;
+    }
+}
