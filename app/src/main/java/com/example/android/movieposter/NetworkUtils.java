d37ad88cf9899a8c2147131c94de42693119bf7b
matewiszt <matezoltan.wiszt@gmail.com>
2018-02-24 22:52:35 +0100

    Add JSON checks to NetworkUtils

--- app/src/main/java/com/example/android/movieposter/NetworkUtils.java
+++ app/src/main/java/com/example/android/movieposter/NetworkUtils.java
@@ -21,7 +21,9 @@ import java.util.List;
 public class NetworkUtils {
 
     //Tag constant for logging
-    private static final String LOG_TAG = "NetworkUtils: ";
+    private static final String LOG_TAG = "NetworkUtils";
+
+    //JSON key constants for parsing the JSON
     private static final String JSON_KEY_RESULTS = "results";
     private static final String JSON_KEY_ID = "id";
     private static final String JSON_KEY_TITLE = "original_title";
@@ -30,6 +32,7 @@ public class NetworkUtils {
     private static final String JSON_KEY_RATING = "vote_average";
     private static final String JSON_KEY_DATE = "release_date";
 
+    //Error String constants
     private static final String URL_ERROR_TEXT = "URL creation failed";
     private static final String SERVER_ERROR_TEXT = "Server error";
     private static final String CONN_ERROR_TEXT = "Connection creation failed";
@@ -37,14 +40,15 @@ public class NetworkUtils {
     private static final String HTTP_ERROR_TEXT = "HTTP request failed";
 
     //Public constructor which will be never used
-    public NetworkUtils(){}
+    public NetworkUtils() {
+    }
 
     /*
      * Fetch the data from the /movie/popular endpoint
      * @param urlString: the URL in String format that we want to query
      * @return List<Movie>: a list of Movie objects
      */
-    public static List<Movie> fetchPopularMoviesData(String urlString){
+    public static List<Movie> fetchPopularMoviesData(String urlString) {
 
         URL requestUrl = createUrl(urlString);
 
@@ -65,10 +69,10 @@ public class NetworkUtils {
      * @param responseJson: the response from the HTTP request in String format
      * @return List<Movie>: a list of Movie objects created from the result
      */
-    private static List<Movie> parsePopularJson(String responseJson){
+    private static List<Movie> parsePopularJson(String responseJson) {
 
         // If the response is empty, just return null
-        if (TextUtils.isEmpty(responseJson)){
+        if (TextUtils.isEmpty(responseJson)) {
             return null;
         }
 
@@ -77,27 +81,50 @@ public class NetworkUtils {
 
         try {
 
-            //Try to parse the result as JSONObject and get the results array
+            // Try to parse the result as JSONObject and get the results array
             JSONObject resultObject = new JSONObject(responseJson);
-            JSONArray resultsArray = resultObject.getJSONArray(JSON_KEY_RESULTS);
 
-            for (int i = 0; i < resultsArray.length(); i++){
+            if (resultObject.has(JSON_KEY_RESULTS)) {
 
-                //Loop through all the results and extract the data from them
-                JSONObject currentMovie = resultsArray.getJSONObject(i);
-                int id = currentMovie.getInt(JSON_KEY_ID);
-                String title = currentMovie.getString(JSON_KEY_TITLE);
-                String imagePath = currentMovie.getString(JSON_KEY_IMAGE);
-                String synopsis = currentMovie.getString(JSON_KEY_SYNOPSIS);
-                double rating = currentMovie.getDouble(JSON_KEY_RATING);
-                String releaseDate = currentMovie.getString(JSON_KEY_DATE);
+                JSONArray resultsArray = resultObject.getJSONArray(JSON_KEY_RESULTS);
 
-                //Create a Movie instance of every result and add them to the movie list
-                Movie movieInstance = new Movie(id, title, imagePath, synopsis, rating, releaseDate);
-                movies.add(movieInstance);
+                for (int i = 0; i < resultsArray.length(); i++) {
+
+                    // Loop through all the results and extract the data from them
+                    JSONObject currentMovie = resultsArray.getJSONObject(i);
+                    int id = 0;
+                    String title = null;
+                    String imagePath = null;
+                    String synopsis = null;
+                    double rating = 0;
+                    String releaseDate = null;
+
+                    if (currentMovie.has(JSON_KEY_ID)) {
+                        id = currentMovie.getInt(JSON_KEY_ID);
+                    }
+                    if (currentMovie.has(JSON_KEY_TITLE)) {
+                        title = currentMovie.getString(JSON_KEY_TITLE);
+                    }
+                    if (currentMovie.has(JSON_KEY_IMAGE)) {
+                        imagePath = currentMovie.getString(JSON_KEY_IMAGE);
+                    }
+                    if (currentMovie.has(JSON_KEY_SYNOPSIS)) {
+                        synopsis = currentMovie.getString(JSON_KEY_SYNOPSIS);
+                    }
+                    if (currentMovie.has(JSON_KEY_RATING)) {
+                        rating = currentMovie.getDouble(JSON_KEY_RATING);
+                    }
+                    if (currentMovie.has(JSON_KEY_DATE)) {
+                        releaseDate = currentMovie.getString(JSON_KEY_DATE);
+                    }
+
+                    // Create a Movie instance of every result and add them to the movie list
+                    Movie movieInstance = new Movie(id, title, imagePath, synopsis, rating, releaseDate);
+                    movies.add(movieInstance);
+                }
             }
 
-        } catch (JSONException e){
+        } catch (JSONException e) {
 
             Log.e(LOG_TAG, JSON_ERROR_TEXT, e);
         }
@@ -112,27 +139,27 @@ public class NetworkUtils {
      * @return String: the String read from the InputStream
      */
 
-    private static String readFromStream(InputStream stream) throws IOException{
+    private static String readFromStream(InputStream stream) throws IOException {
 
         StringBuilder responseString = new StringBuilder();
 
-        //If we don't have an input stream, we cannot get the response String
-        if (stream == null){
+        // If we don't have an input stream, we cannot get the response String
+        if (stream == null) {
             return null;
         }
 
-        //Create a new InputStreamReader and a BufferedReader
+        // Create a new InputStreamReader and a BufferedReader
         InputStreamReader streamReader = new InputStreamReader(stream, Charset.forName("UTF-8"));
         BufferedReader bufferedReader = new BufferedReader(streamReader);
 
-        //Read the buffered response from line to line and append it to the StringBuilder
+        // Read the buffered response from line to line and append it to the StringBuilder
         String line = bufferedReader.readLine();
-        while(line != null){
+        while (line != null) {
             responseString.append(line);
             line = bufferedReader.readLine();
         }
 
-        //Convert the StringBuilder to a String and return it
+        // Convert the StringBuilder to a String and return it
         return responseString.toString();
 
     }
@@ -146,7 +173,7 @@ public class NetworkUtils {
 
         String responseString = null;
 
-        //If we don't have URL, we won't have a response, so return null
+        // If we don't have URL, we won't have a response, so return null
         if (url == null) {
             return null;
         }
@@ -156,18 +183,19 @@ public class NetworkUtils {
 
         try {
 
-            //Set the parameters of the connection and connect
+            // Set the parameters of the connection and connect
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setConnectTimeout(15000);
             connection.setReadTimeout(15000);
             connection.connect();
 
-            if (connection.getResponseCode() == 200){
+            if (connection.getResponseCode() == 200) {
 
-                //If the connection is successful, get the input stream and read the response String from it
+                // If the connection is successful, get the input stream and read the response String from it
                 responseStream = connection.getInputStream();
                 responseString = readFromStream(responseStream);
+
             } else {
                 Log.e(LOG_TAG, SERVER_ERROR_TEXT);
             }
@@ -176,13 +204,13 @@ public class NetworkUtils {
             Log.e(LOG_TAG, CONN_ERROR_TEXT, e);
         } finally {
 
-            //If we are not disconnected yet, disconnect
-            if (connection != null){
+            // If we are not disconnected yet, disconnect
+            if (connection != null) {
                 connection.disconnect();
             }
 
-            //If the input stream is not closed yet, close it
-            if (responseStream != null){
+            // If the input stream is not closed yet, close it
+            if (responseStream != null) {
                 responseStream.close();
             }
 
@@ -197,13 +225,13 @@ public class NetworkUtils {
      * @param urlString: the String from which we want to create a URL
      * @return URL: the properly formatted URL
      */
-    private static URL createUrl(String urlString){
+    private static URL createUrl(String urlString) {
         URL url = null;
 
         try {
             url = new URL(urlString);
 
-        } catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             Log.e(LOG_TAG, URL_ERROR_TEXT, e);
         }
         return url;
