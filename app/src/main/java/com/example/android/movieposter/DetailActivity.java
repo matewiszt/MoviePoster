6823c5fa4b65d4169180f9ce81e0e2768a32af0f
matewiszt <matezoltan.wiszt@gmail.com>
2018-03-11 12:10:48 +0100

    Add new API calls to DetailActivity

--- app/src/main/java/com/example/android/movieposter/DetailActivity.java
+++ app/src/main/java/com/example/android/movieposter/DetailActivity.java
@@ -1,14 +1,24 @@
 package com.example.android.movieposter;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.android.movieposter.network.MovieService;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -19,6 +29,8 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.detail_rating_tv) TextView mRatingTextView;
     @BindView(R.id.detail_date_tv) TextView mReleaseDateTextView;
 
+    public int mId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +40,12 @@ public class DetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         // Initialize the local variables for the Intent extras
-        int id = 0;
         String title = null;
         String image = null;
         String synopsis = null;
         String rating = null;
         String date = null;
-        Movie movie = null;
+        Movie movie;
 
         // If we have extras, check if the extras have the following keys and store them in the variables
         Bundle extras = getIntent().getExtras();
@@ -42,6 +53,7 @@ public class DetailActivity extends AppCompatActivity {
 
             if (extras.containsKey(MainActivity.DETAIL_MOVIE_KEY)) {
                 movie = extras.getParcelable(MainActivity.DETAIL_MOVIE_KEY);
+                mId = movie.getId();
                 title = movie.getTitle();
                 image = movie.getImageFullPath();
                 synopsis = movie.getSynopsis();
@@ -60,5 +72,76 @@ public class DetailActivity extends AppCompatActivity {
         mSynopsisTextView.setText(synopsis);
         mRatingTextView.setText(rating);
         mReleaseDateTextView.setText(date);
+
+        // Initialize a ConnectivityManager to get the active network info
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        // If the device is connected to the internet, initialize a Loader
+        if (isConnected) {
+
+            loadReviews();
+            loadTrailers();
+
+        }
+    }
+
+    private void loadReviews() {
+
+        // Make a call with the endpoint
+        Call<Reviews> call = MovieService.getReviews(mId);
+
+        if (call == null) {
+            return;
+        }
+
+        call.enqueue(new Callback<Reviews>() {
+            @Override
+            public void onResponse(@NonNull Call<Reviews> call, @NonNull Response<Reviews> response) {
+                Reviews reviews = response.body();
+
+                if (reviews != null) {
+
+                    List<Review> reviewList = reviews.items;
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Reviews> call, @NonNull Throwable t) {
+                call.cancel();
+            }
+        });
+
+    }
+
+    private void loadTrailers() {
+
+        // Make a call with the endpoint
+        Call<Trailers> call = MovieService.getTrailers(mId);
+
+        if (call == null) {
+            return;
+        }
+
+        call.enqueue(new Callback<Trailers>() {
+            @Override
+            public void onResponse(@NonNull Call<Trailers> call, @NonNull Response<Trailers> response) {
+                Trailers trailers = response.body();
+
+                if (trailers != null) {
+
+                    List<Trailer> trailerList = trailers.items;
+
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Trailers> call, @NonNull Throwable t) {
+                call.cancel();
+            }
+        });
+
     }
 }
